bb7c923d635ff572e11b6d0c7c578d3f90a8c246
G-BAPRO\jroclaw <jroclaw@provincianet.com.ar>
2024-04-18 01:23:55 -0300

    jroclaw->se agrego el footer y se creo el boton de top

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-const navLinks = document.querySelectorAll('.navbar-toggler')
-const menuToggle = document.getElementById('navbarToggleExternalContent')
-const bsCollapse = bootstrap.Collapse(menuToggle, {toggle:false})
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
